b85f3bf315a9956deff23615e1b2462ba222961c
MohamedBoukthir <medboukthir7@gmail.com>
2024-01-10 14:24:47 +0100

    StringAndArrayString

---
+++ src/String/StringAndArrayString.java
@@ -0,0 +1,94 @@
+package String;
+
+
+// Java - String Class
+// YOU CAN FIND ANYTHING ABOUT THE STRING CLASS HERE : https://docs.oracle.com/javase/8/docs/api/java/lang/String.html
+class StringAndArrayString {
+    public static void main(String[] args) {
+//        String str = "Hello";
+
+        // Concat two Strings
+//        str = str + " World";
+//        // Or Using String Build In Functions
+//        str = str.concat(" World");
+//        System.out.println(str);
+
+//        // Compare String
+//        String s1 = "ABCD"; // D = 68 (ASCII CODE)
+//        String s2 = "ABCe"; // e = 101 (ASCII CODE)
+//        // to know the ascii code of any char ->  System.out.println((int) 'A');
+//        // So 68 - 101 = -33 (D - e)
+//        System.out.println(s1.compareTo(s2));
+//            /*
+//            if s1>s2 = +num
+//            if s1<s2 = -num
+//            if s1=s2 = 0
+//            */
+
+//        // Trim String
+//        String str = "   Hello   ";
+//        System.out.println(str + " World");
+//        // Trim will remove any spaces in the String
+//        System.out.println(str.trim() + " World");
+
+//        // toUpperCase And ToLowerCase
+//        String str = "HelLo , I M moHAmED.";
+//        // to make all the String with Upper Case
+//        System.out.println(str.toUpperCase());
+//        // to make all the String with Lower Case
+//        System.out.println(str.toLowerCase());
+
+//        // Cast an Variable To String
+//        int x = 10;
+//        String str = String.valueOf(x);
+//        // OR
+//        String str = x + "";
+//        System.out.println(str);
+
+//        // Ignore Case
+//        String s1 = "HELLO";
+//        String s2 = "hello";
+//        System.out.println(s1.equalsIgnoreCase(s2)); // ignore the case of the string (If UPPER Or lower)
+
+//        // Replace Something in String
+//        String str = "I'm Senior JAVA Dev";
+//        String NewString = str.replace("Senior", "Junior"); // String NewString = str.replace("OLD PART", "NEW PART")
+//        System.out.println(NewString);
+
+//        // If Contains Method
+//        String str = "I want to be a software engineer.";
+//        System.out.println(str.contains("I to be")); // false (Because the absence of word "Want")
+//        System.out.println(str.contains("software engineer")); // true
+//        System.out.println(str.contains("I")); // true
+
+//         // Start WWith And End With
+//        String str = "Of Course you can use JAVA in Web Dev";
+//        // endsWith
+//        System.out.println(str.endsWith("Dev")); // true
+//        System.out.println(str.endsWith("dev")); // false
+//        System.out.println(str.endsWith("v")); // true
+//        // startWith
+//        System.out.println(str.startsWith("O")); // true
+//        System.out.println(str.startsWith("of")); // false
+//        System.out.println(str.startsWith("Course")); // false
+
+//        // Substring
+//        String str = "Hello";
+//        System.out.println(str.substring(2)); // output : llo
+//        System.out.println(str.substring(2, 4)); // output : ll (Start from index 2 and stop in index 4)
+
+//        // Split
+//        String str = "AA-BB-CC-DD-EE";
+//        for (String val : str.split("-")
+//        ) {
+//            System.out.print(val); // output = AABBCCDDEE
+//        }
+
+//        // print Array of String
+//        String str[] = {"Mohamed", "JAVA", "Software", "Developer"};
+//        for (int i = 0; i < str.length; i++) {
+//            System.out.print(str[i] + " ");
+//        }
+
+    }
+}
